242e388904d970fe32801313c4bddf62584705ca
ProgramingHub(dont know what i m dong :D) <111310544+Spooderman177@users.noreply.github.com>
2022-11-20 20:55:22 +0530

    Editing

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ firebase
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
       var uid = user.uid;
+      document.getElementById("login_li").style.display = "none"
       
       document.getElementById("main-body").style.display = "initial";
       document.getElementById("login-register").style.display = "none";
@@ -27,6 +28,7 @@ firebase
       if(user != null)
       {
         var email_id = user.email;
+        document.getElementById("UserName").textContent = email_id
       }
       // ...
     } else {
@@ -66,8 +68,64 @@ login.addEventListener("click", (e) => {
     });
 });
 
+const register = document.getElementById("register_btn");
+register.addEventListener('click',(e) => {
+    e.preventDefault();
+    console.log("Rgister user")
+    Register();
+})
+
 function logout()
 {
     var user = firebase.auth().currentUser;
     window.alert("Logout")
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        location.reload();
+      }).catch((error) => {
+        // An error happened.
+      });
 }
+
+function Register()
+{
+    const email = document.getElementById("registerEmail").value;
+    const password = document.getElementById("registerPassword").value;
+    const name = document.getElementById("registerName").value;
+    const username = document.getElementById("registerUsername").value;
+    const repeat_pss = document.getElementById("registerRepeatPassword").value
+    if(repeat_pss == password)
+    {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    db.collection("Users").add({
+        name:name,
+        email:email,
+        username:username,
+    })
+    .then((docRef) => {
+        docRef.update({
+            id: docRef.id,
+        })
+    })
+    .catch((e) => {
+        console.log(e.code,e.message);
+    })
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(error.code,error.message);
+    window.alert(error.message)
+    // ..
+  });
+}
+else {
+    window.alert("Password Does Not match Re- Enter Password ")
+}
+}
+
+window.onclose
